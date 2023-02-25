26fd8fc97360b564863ce01c5453690f1c372141
rudolf <rudigard15@gmail.com>
2023-02-25 21:55:03 +0100

    error input handling done

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -11,6 +11,8 @@ export class FormComponent implements OnInit {
   @Input() title: string = "Language";
   @Input() backButtonUrl: string = "/languages";
 
+  error: boolean = false;
+
   @Output() submitLangForm: EventEmitter<Word> = new EventEmitter();
   
   inputValues: Word = {
@@ -24,12 +26,34 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  errorHandler(word: string): boolean{
+   var  notAllowedSigns = ["<", ">", ".", "!", "?", "#", "@", "$", "%", "^", "&", "*", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+    var result: boolean = false;
+
+   notAllowedSigns.forEach(element => {
+      if(word.includes(element)){
+        result = true;
+      } 
+   });
+
+   return result;
+  }
+
   submitForm(){
     //console.log(`${this.inputValues.word} / ${this.inputValues.translation} `);
-
-    this.submitLangForm.emit(this.inputValues);
+    //  error handling
+    if(this.errorHandler(this.inputValues.word) || this.errorHandler(this.inputValues.translation) || 
+      this.inputValues.word == "" || this.inputValues.translation == ""){
+      this.error =true;
+      this.inputValues.word = "";
+      this.inputValues.translation = "";
+    } else {
+      this.error = false;
+      this.submitLangForm.emit(this.inputValues);
 
     this.inputValues.word = "";
     this.inputValues.translation = "";
+    }
   }
 }
